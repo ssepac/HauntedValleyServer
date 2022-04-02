605c9ba94fb11af32eb120d27a4d9c58de17fac2
ssepac <ssepac2@illinois.edu>
2022-04-02 00:45:17 -0600

    fix: basic fixes to support multiplayer

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
 const dgram = require("dgram");
 const luxon = require("luxon");
-const dod = require('deep-object-diff')
-const { MESSAGE_TYPES, DEFAULT_INACTIVITY_TIME, DEFAULT_SUBSCRIPTION_EXPIRY_TIME } = require("./constants");
-const {checkOnline, getSubscriberName} = require('./util')
+const dod = require("deep-object-diff");
+const {
+  MESSAGE_TYPES,
+  DEFAULT_INACTIVITY_TIME,
+  DEFAULT_SUBSCRIPTION_EXPIRY_TIME,
+} = require("./constants");
+const { checkOnline, getSubscriberName } = require("./util");
 
 const server = dgram.createSocket("udp4");
 
 //Includes dummy first value
-const playerInfoMap = {
-  "0xdummy_address": {
-    x: 450,
-    y: 450,
-    mapId: "map1",
-    direction: "WEST",
-  },
-};
-let playerInfoMapOld={
-  "0xdummy_address": {
-    x: 450,
-    y: 450,
-    mapId: "map1",
-    direction: "WEST",
-  },
-}
+const playerInfoMap = {};
+let playerInfoMapOld = {};
 let playerStatusMap = {};
 const PORT = 3000;
-const HOST = "192.168.1.174";
+const HOST = "192.168.0.24";
 
 server.on("error", (err) => {
   console.log(`server error:\n${err.stack}`);
@@ -38,12 +28,10 @@ server.on("connect", () => {
 
 server.on("message", (msg, rinfo) => {
   const parsedMsg = JSON.parse(msg);
-  console.log(parsedMsg)
 
   //TODO Split this into different files and/or methods
   switch (parsedMsg.messageType) {
     case MESSAGE_TYPES.POSITION_UPDATE:
-
       //Upsert player status
       playerStatusMap[parsedMsg.address] = {
         lastActivity: luxon.DateTime.now().toMillis(),
@@ -59,6 +47,7 @@ server.on("message", (msg, rinfo) => {
         mapId: parsedMsg.mapId,
         direction: parsedMsg.direction,
         isWalking: parsedMsg.isWalking,
+        messageType: MESSAGE_TYPES.POSITION_UPDATE
       };
       //console.log(JSON.stringify(playerInfoMap));
       //server.send(JSON.stringify(playerInfoMap), rinfo.port, rinfo.address);
@@ -72,14 +61,14 @@ server.on("message", (msg, rinfo) => {
         connectionInfo: {
           port: rinfo.port,
           address: rinfo.address,
-          persistent: true
+          persistent: true,
         },
       };
-      console.log(`${rinfo.address} subscribed.`)
+      console.log(`${rinfo.address} subscribed.`);
       break;
     case MESSAGE_TYPES.UNSUBSCRIBE:
-      delete playerStatusMap[rinfo.address]
-      console.log(`${rinfo.address}:${rinfo.port} unsubscribed.`)
+      delete playerStatusMap[rinfo.address];
+      console.log(`${rinfo.address}:${rinfo.port} unsubscribed.`);
       break;
   }
 });
@@ -93,27 +82,52 @@ server.bind(PORT, HOST);
 
 //Sends player coord map to all players.
 setInterval(() => {
-  Object.keys(playerStatusMap).forEach((player) => {
-    const diff = dod.diff(playerInfoMapOld, playerInfoMap)
-    //console.log(JSON.stringify(diff))
-    server.send(JSON.stringify(diff), playerStatusMap[player].connectionInfo.port, playerStatusMap[player].connectionInfo.address);
-    playerInfoMapOld = {...playerInfoMap}
-  });
-}, 15);
+
+  //find which players changed coordinates/location
+  const diff = dod.diff(playerInfoMapOld, playerInfoMap);
+
+  //If any of the players data changed, build a broadcast object that contains all new changed player locations and broadcast to everyone online.
+  if (Object.keys(diff).length > 0) {
+    let broadcast = {};
+    Object.keys(diff).forEach((key) => {
+      broadcast[key] = playerInfoMap[key];
+    });
+
+    Object.keys(playerStatusMap).forEach(player=> server.send(JSON.stringify(broadcast), playerStatusMap[player].connectionInfo.port, playerStatusMap[player].connectionInfo.address))
+  }
+
+  playerInfoMapOld = { ...playerInfoMap };
+}, 15); //change back to 15
 
 //Remove inactive players and expired subscriptions from "mailing list".
-setInterval(()=>{
+setInterval(() => {
   Object.keys(playerStatusMap).forEach((player) => {
-
     /** Players will log out after 10 seconds of no update from client. */
-    if(!playerStatusMap[player].connectionInfo.persistent && !checkOnline(DEFAULT_INACTIVITY_TIME, luxon.DateTime.now().toMillis(), playerStatusMap[player].lastActivity)){
-      console.log(`Player at ${playerStatusMap[player].connectionInfo.address}:${playerStatusMap[player].connectionInfo.port} went offline.`)
-      delete playerStatusMap[player]
-    }
+    if (
+      !playerStatusMap[player].connectionInfo.persistent &&
+      !checkOnline(
+        DEFAULT_INACTIVITY_TIME,
+        luxon.DateTime.now().toMillis(),
+        playerStatusMap[player].lastActivity
+      )
+    ) {
+      console.log(
+        `Player at ${playerStatusMap[player].connectionInfo.address}:${playerStatusMap[player].connectionInfo.port} went offline.`
+      );
+      delete playerStatusMap[player];
+    } else if (
     /** Subscriptions will persist up to an hour (3.6e6 ms) before terminating. */
-    else if(playerStatusMap[player].connectionInfo.persistent && !checkOnline(DEFAULT_SUBSCRIPTION_EXPIRY_TIME, luxon.DateTime.now().toMillis(), playerStatusMap[player].lastActivity)){
-      console.log(`Subscriber at ${playerStatusMap[player].connectionInfo.address}:${playerStatusMap[player].connectionInfo.port} was removed via expiration.`)
-      delete playerStatusMap[player]
+      playerStatusMap[player].connectionInfo.persistent &&
+      !checkOnline(
+        DEFAULT_SUBSCRIPTION_EXPIRY_TIME,
+        luxon.DateTime.now().toMillis(),
+        playerStatusMap[player].lastActivity
+      )
+    ) {
+      console.log(
+        `Subscriber at ${playerStatusMap[player].connectionInfo.address}:${playerStatusMap[player].connectionInfo.port} was removed via expiration.`
+      );
+      delete playerStatusMap[player];
     }
-  })
-}, 1000)
+  });
+}, 1000);
--- tests/multiplayer/testplayer.js
+++ tests/multiplayer/testplayer.js
@@ -1,12 +1,13 @@
 const dgram = require("dgram");
-const MIN = 200;
-const MAX = 300;
+const { MESSAGE_TYPES } = require("../../constants");
+const MIN = 25;
+const MAX = 200;
 const UPDATE_MS = 33.33;
 const WALKING_SPEED = 32; //pixel per second
 const INCREMENT = WALKING_SPEED * (UPDATE_MS / 1000);
-const HOST = "192.168.1.174";
+const HOST = "0.0.0.0";
 const PORT = 3000;
-const y = 200;
+const y = 400;
 let x = MIN;
 let increase = true;
 
@@ -19,17 +20,18 @@ setInterval(() => {
   const direction = increase ? "EAST" : "WEST";
 
   const msg = {
-    x: newX,
-    y,
+    address: "0x1231B0d15a11912959092635BE9803EDb2398EC3",
+    xCoord: newX,
+    yCoord: y,
     mapId: "map1",
     direction,
     isWalking: true,
-    messageType: "POSITION_UPDATE",
+    messageType: MESSAGE_TYPES.POSITION_UPDATE,
   };
 
   const msgStr = JSON.stringify(msg);
 
-  client.send(msgStr, 0, msgStr.length, PORT, HOST);
+  client.send(msgStr, PORT, HOST, (err, bytes) => console.log(msgStr));
 
   if (newX <= MIN) increase = true;
   if (newX >= MAX) increase = false;
